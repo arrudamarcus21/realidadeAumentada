8eb0129a1aed509dd53514f5a283fee1f0a08965
arrudamarcus21 <45611501+arrudamarcus21@users.noreply.github.com>
2019-01-16 14:00:47 -0300

    Update artoolkit.three.js

--- js/artoolkit.three.js
+++ js/artoolkit.three.js
@@ -190,7 +190,7 @@
 			return obj;
 		};
 
-		/**
+		
 			Creates a Three.js marker Object3D for the given NFT marker UID.
 			The marker Object3D tracks the NFT marker when it's detected in the video.
 
@@ -205,7 +205,7 @@
 			@param {number} markerUID The UID of the marker to track.
 			@param {number} markerWidth The width of the marker, defaults to 1.
 			@return {THREE.Object3D} Three.Object3D that tracks the given marker.
-		*/
+		
 		ARController.prototype.createThreeNFTMarker = function(markerUID, markerWidth) {
 			this.setupThree();
 			var obj = new THREE.Object3D();
